8076fd96f33fc3b0c0560939479b86085fab85df
alexmquinlan <alexquinlan1@gmail.com>
2017-01-12 21:28:57 -0500

    changed back the color scheme, added about page and some extra functionality

---
+++ assets/js/acgl.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+	// set heights on window load //
+	$('#welcome').height($(window).height() + 80);
+	$('#main-text').height($(window).height() - 120);
+	$('#about-content').height($('#about-acgl').height() - $('#about-header').height());
+
+
+	setTimeout(function() {
+		$('#sub-text').css('opacity',0).animate({opacity:1}, 1000);
+	}, 1500);
+
+
+	$('#nav').affix({
+        offset: { top: $('#nav').offset().top }
+    }).on('affix.bs.affix', function () {
+        $('#navLinks-logo').fadeIn();
+        $('#logo').removeClass('logo-hidden');
+    }).on('affix-top.bs.affix', function () {
+        $('#navLinks-logo').fadeOut();
+        $('#logo').addClass('logo-hidden');
+    });
+
+
+    $(".navLinks").click(function() {
+    	var name = $(this).attr('name');
+	    $('html, body').animate({
+	        scrollTop: $("#" + name).offset().top - 80
+	    }, 1500);
+	    return false;
+	});
+
+
+    if ($('#nav').hasClass('affix')) {
+    	$('#navLinks-logo').show();
+    	$('#logo').removeClass('logo-hidden');
+    }
+
+    if ($(this).scrollTop() > $('#about').position().top - 120) {
+		$('.about-underline').css('width',0).animate({width:"40%"}, 1000, 'swing');
+        return false;
+    }
+
+
+	$(window).scroll(function(){
+        if ($(this).scrollTop() > ($('#about').position().top - 120)) {
+    		$('.about-underline').css('width',0).animate({width:"40%"}, 1000, 'swing');
+    		$('#linkabout').addClass('active');
+            return false;
+        }
+        else {
+    		$('#linkabout').removeClass('active');        	
+        }
+    });
+
+
+});
